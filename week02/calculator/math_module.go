84575b31238673b52e05a3781f517132eaecb12c
kmin1231 <kmin91143@gmail.com>
2024-10-13 07:10:10 +0900

    update on 24.10.13.

---
+++ week02/calculator/math_module.go
@@ -0,0 +1,24 @@
+// calculator/math_module.go
+
+package calculator
+import "fmt"
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a *  b
+}
+
+func Divide(a int, b int) int {
+	if (b == 0) {
+		fmt.Println("Error: division by ZERO!")
+		return 0
+	}
+	return a / b
+}
