a37b52ad1268b821f47430ebdfd3d27912ec3863
kmin1231 <kmin91143@gmail.com>
2024-11-10 01:27:46 +0900

    Chapter 16 files updated

---
+++ week05/Chapter16/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+	"github.com/kmin1231/go_server_session/weke05/Chapter16/config"
+)
+
+func main() {
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	s := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if err := s.Serve(l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	return eg.Wait()
+}
