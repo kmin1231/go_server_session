b7eb31808d49d755237cea608eb1d0855f93aba2
kmin1231 <kmin91143@gmail.com>
2024-11-16 06:23:54 +0900

    Chapter 18 files updated

--- week06/Chapter18/handler/add_task_test.go
+++ week06/Chapter18/handler/add_task_test.go
@@ -48,9 +48,13 @@ func TestAddTask(t *testing.T) {
 				bytes.NewReader(testutil.LoadFile(t, tt.reqFile)),
 			)
 
-			sut := AddTask{Store: &store.TaskStore{
-				Tasks: map[entity.TaskID]*entity.Task{},
-			}, Validator: validator.New()}
+			// sut: System Under Test
+			sut := AddTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+				Validator: validator.New(),
+			}
 			sut.ServeHTTP(w, r)
 
 			resp := w.Result()
