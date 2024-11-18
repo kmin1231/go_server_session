ed2c735a522e2b6fcbc360e6f4f104c34247bf03
kmin1231 <kmin91143@gmail.com>
2024-11-18 23:35:36 +0900

    Chapter 19 files uploaded

---
+++ week07/Chapter19/handler/list_task.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/kmin1231/go_server_session/week06/Chapter18/entity"
+	"github.com/kmin1231/go_server_session/week06/Chapter18/store"
+)
+
+type ListTask struct {
+	DB   *sqlx.DB
+	Repo *store.Repository
+}
+
+type task struct {
+	ID     entity.TaskID     `json:"id"`
+	Title  string            `json:"title"`
+	Status entity.TaskStatus `json:"status"`
+}
+
+func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tasks, err := lt.Repo.ListTasks(ctx, lt.DB)
+	if err != nil {
+		RespondJSON(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	rsp := []task{}
+	for _, t := range tasks {
+		rsp = append(rsp, task{
+			ID:     t.ID,
+			Title:  t.Title,
+			Status: t.Status,
+		})
+	}
+	RespondJSON(ctx, w, rsp, http.StatusOK)
+}
