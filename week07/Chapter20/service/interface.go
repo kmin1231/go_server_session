4e186feebc3f77ac8636dbbd4b15a4219142e26c
kmin1231 <kmin91143@gmail.com>
2024-11-20 00:56:46 +0900

    Chapter 20 files updated

---
+++ week07/Chapter20/service/interface.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"context"
+
+	"github.com/kmin1231/go_server_session/week07/Chapter20/entity"
+	"github.com/kmin1231/go_server_session/week07/Chapter20/store"
+)
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskLister
+type TaskAdder interface {
+	AddTask(ctx context.Context, db store.Execer, t *entity.Task) error
+}
+
+type TaskLister interface {
+	ListTasks(ctx context.Context, db store.Queryer) (entity.Tasks, error)
+}
+
+type UserRegister interface {
+	RegisterUser(ctx context.Context, db store.Execer, u *entity.User) error
+}
